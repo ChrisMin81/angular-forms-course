573c3e14e3f1ec853a3098d0e14c8335c547f4be
Christian Minatti <christian@minatti.tirol>
2023-01-13 13:34:15 +0100

    basic reactive driven forms finished

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, NonNullableFormBuilder, Validators} from "@angular/forms";
+import {PasswordStrengthDirective} from "../directives/password-strength.directive";
+import {createPasswordStrengthValidator} from "../validators/password-strength.validator";
 
 
 @Component({
@@ -7,15 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-reactive.component.css']
 })
 export class LoginReactiveComponent implements OnInit {
+  form = this.fb.group({
+    email: ['', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur',
+      asyncValidators: []
+    }],
+    password: ['', [Validators.required, Validators.minLength(8), createPasswordStrengthValidator()], []],
+  })
 
+  logLogin() {
+    const formValue = this.form.value
 
-  constructor() {
+  }
 
-
+  constructor(private fb: NonNullableFormBuilder) {
   }
 
   ngOnInit() {
 
   }
 
+  get email() {
+    return this.form.controls['email']
+  }
+
+  get password() {
+    return this.form.controls['password']
+  }
+
+  reset() {
+    this.form.reset()
+    console.log(this.form.value)
+  }
 }
